53596cb05bb99f3c7ba56bd0ecc1cc78888a634a
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-03-11 23:44:16 +0700

    dev

--- setup.py
+++ setup.py
@@ -8,14 +8,12 @@ from datetime import datetime
 
 CURRENT_DIR = os.getcwd()
 LOGGER_CONFIG = join(CURRENT_DIR, 'logging_config.yaml')
-
 class Folder:
     RAW         = join(CURRENT_DIR, "raw/")
     EXPORT      = join(CURRENT_DIR, "export/")
     TMP         = join(CURRENT_DIR, "tmp/dd_export/")
     LOG         = join(CURRENT_DIR, "tmp/log/")
     FILE        = ['ADM.txt', 'BOS.xlsx', 'CUM.xls', 'DocImage.txt', 'ICAS-NCR.xlsx', 'IIC.xlsx', 'LDS-P_UserDetail.txt', 'Lead-Management.xlsx', 'MOC.xlsx']
-
 
 class ArgumentParams:
     SHORT_NAME = 'short_name'
@@ -27,9 +25,37 @@ class ArgumentParams:
     TYPE = 'type'
     CHOICES = 'choices'
 
+def setup_log():
+    config_yaml  = None
+    date = datetime.today().strftime("%d%m%Y")
+    log_name = f'log_{date}.log'
+
+    if os.path.exists(LOGGER_CONFIG):
+        with open(LOGGER_CONFIG, 'rb') as logger:
+            config_yaml  = yaml.safe_load(logger.read())
+            for i in (config_yaml["handlers"].keys()):
+                if 'filename' in config_yaml['handlers'][i]:
+                    log_path = config_yaml["handlers"][i]["filename"]
+                    log_file = log_path + log_name
+            config_yaml["handlers"][i]["filename"] = log_file
+
+            logging.config.dictConfig(config_yaml)
+    else:
+        raise Exception(f"Yaml file file_path: '{LOGGER_CONFIG}' doesn't exist")
+
+def setup_folder():
+    _folders = [value for name, value in vars(Folder).items() if isinstance(value, str) and not name.startswith('_')]
+    for folder in _folders:
+        os.makedirs(folder, exist_ok=True)
+
+def clear_folder():
+    _folders = [value for name, value in vars(Folder).items() if isinstance(value, str) and not name.startswith('_') and value.endswith('dd_export/')]
+    for folder in _folders:
+        shutil.rmtree(folder)
 
 class setup_parser:
     def __init__(self):
+        
         self.parser = argparse.ArgumentParser()
         self.add_arguments()
         self.param = self.parser.parse_args()
@@ -70,34 +96,3 @@ class setup_parser:
                 else:
                     self.parser.add_argument(short_name, name, help=description, required=required,
                                         default=default, action=action, choices=choices)
-
-def setup_log():
-
-    config_yaml  = None
-    date = datetime.today().strftime("%d%m%Y")
-    log_name = f'log_{date}.log'
-
-    if os.path.exists(LOGGER_CONFIG):
-        with open(LOGGER_CONFIG, 'rb') as logger:
-            config_yaml  = yaml.safe_load(logger.read())
-            for i in (config_yaml["handlers"].keys()):
-                if 'filename' in config_yaml['handlers'][i]:
-                    log_path = config_yaml["handlers"][i]["filename"]
-                    log_file = log_path + log_name
-            config_yaml["handlers"][i]["filename"] = log_file
-
-            logging.config.dictConfig(config_yaml)
-    else:
-        raise Exception(f"Yaml file file_path: '{LOGGER_CONFIG}' doesn't exist")
-
-
-def setup_folder():
-    _folders = [value for name, value in vars(Folder).items() if isinstance(value, str) and not name.startswith('_')]
-    for folder in _folders:
-        os.makedirs(folder, exist_ok=True)
-
-
-def clear_folder():
-    _folders = [value for name, value in vars(Folder).items() if isinstance(value, str) and not name.startswith('_') and value.endswith('dd_export/')]
-    for folder in _folders:
-        shutil.rmtree(folder)
