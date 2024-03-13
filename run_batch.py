75470b33878e876a9450e900a46cf1884741b3a8
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-03-13 23:04:02 +0700

    for test

--- run_batch.py
+++ run_batch.py
@@ -2,7 +2,6 @@ import glob
 import warnings
 import logging
 import pandas as pd
-import numpy as np
 from pathlib import Path
 from verify_data import validate_files
 from exception import CustomException
@@ -83,7 +82,7 @@ class convert_2_file(validate_files):
             mock_data = [['ApplicationCode',	'AccountOwner', 'AccountName',	'AccountType',	'EntitlementName',	'SecondEntitlementName','ThirdEntitlementName', 'AccountStatus',	'IsPrivileged',	'AccountDescription',
                         'CreateDate','LastLogin','LastUpdatedDate',	'AdditionalAttribute'],
                         [1,2,3,4,5,6,7,8,9,10,args[0].batch_date.strftime('%Y-%m-%d'),12, args[0].date,14],
-                        # [15,16,17,18,19,20,21,22,23,24,args[0].batch_date.strftime('%Y-%m-%d'),26, args[0].date,28],
+                        [15,16,17,18,19,20,21,22,23,24,args[0].batch_date.strftime('%Y-%m-%d'),26, args[0].date,28],
                         ]
             df = pd.DataFrame(mock_data)
             df.columns = df.iloc[0].values
@@ -237,7 +236,7 @@ class convert_2_file(validate_files):
             for row in sheet.iter_rows():
                 if not all(cell.value for cell in row):
                     sheet.delete_rows(row[0].row, 1)
-                    remove_row(sheet)
+                    remove_row_empty(sheet)
 
         for key in self.__log:
             try:
